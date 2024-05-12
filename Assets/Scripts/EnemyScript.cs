c5ec49fdb89f27d78f1d3582a635910a330ed66d
aissaiyah <aissaiyah.bennett.sia@gmail.com>
2024-05-12 19:15:15 -0400

    Enemy spawning is completed

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,6 +7,7 @@ public class EnemyScript : MonoBehaviour
     public Transform playerTransform;
     public GameObject player;
     public int health;
+    public bool detect;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,22 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(this.transform.position, playerTransform.position, 0.01f);
+        if (detect)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, playerTransform.position, 0.01f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            detect = true;
+            
+
+        }
+
     }
 }
+
+
